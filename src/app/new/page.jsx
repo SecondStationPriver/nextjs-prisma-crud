8b178c4c345c18ced1168bc52b14b669993320fe
SecondStation Priver <second.workstation@priver.app>
2024-02-15 15:58:46 -0400

    CRUD con prisma

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,5 +1,72 @@
-function NewPage() {
-  return <div>Formulario de tareas</div>
+'use client'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+function NewPage({ params }) {
+  const router = useRouter()
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    fetch(`/api/tasks/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTitle(data.title)
+        setDescription(data.description)
+      })
+  }, [])
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    if (params.id) {
+      console.log('updating')
+    } else {
+      const res = await fetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json()
+    }
+
+    //router.push('/')
+  }
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <form className='bg-slate-800 p-10 w-2/4' onSubmit={onSubmit}>
+        <label htmlFor='title' className='font-bold text-sm'>
+          Titulo de la tarea
+        </label>
+        <input
+          type='text'
+          id='title'
+          className='border border-gray-400 p-2 mb-4 w-full text-black'
+          placeholder='Titulo'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <label htmlFor='description' className='font-bold text-sm'>
+          Descripcion de la tarea
+        </label>
+        <textarea
+          rows='3'
+          id='description'
+          className='border border-gray-400 p2 mb-4 w-full text-black'
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px4 rounded'>
+          Crear
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default NewPage
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { prisma } from '@/libs/prisma'
+import TaskCard from '@/components/TaskCard'
 
-function HomePage() {
-  return <div>Tareas</div>
+async function loadTasks() {
+  return await prisma.task.findMany()
+}
+
+async function HomePage() {
+  const tasks = await loadTasks()
+  console.log(tasks)
+
+  return (
+    <section className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-3 mt-10'>
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id} />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default HomePage
